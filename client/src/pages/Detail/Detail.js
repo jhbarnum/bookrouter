4369dba7fbd7b7514fa3d5fc3c3dc013d8dc8206
Jared <jhbarnum@gmail.com>
2018-05-04 23:39:59 -0400

    youtube embed

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -102,3 +102,9 @@ class Detail extends Component {
 }
 
 export default Detail;
+
+
+<iframe width="560" height="315" 
+src="https://www.youtube.com/embed/2-afjrhl6ko" 
+frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+</iframe>
